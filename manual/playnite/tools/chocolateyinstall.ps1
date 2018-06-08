9117383301748d659d28a9e3fbc3ec85a0cfe269
William Quinn <william@hyp5r.io>
2018-06-08 09:46:59 -0500

    (playnite) Updated nuspec and chocolateyinstall according to Chocolatey reviewer

--- manual/playnite/tools/chocolateyinstall.ps1
+++ manual/playnite/tools/chocolateyinstall.ps1
@@ -2,20 +2,16 @@ $ErrorActionPreference = 'Stop'
 
 $packageName= 'playnite'
 $url        = 'https://github.com/JosefNemec/Playnite/releases/download/4.30/Playnite430.exe'
-$url64      = 'https://github.com/JosefNemec/Playnite/releases/download/4.30/Playnite430.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'playnite*'
 
   checksum      = 'EC123F7A2BB7D0EC2B0F176A1BB7F855137BB4423CD78560E9C0C07C2EB5692C'
   checksumType  = 'sha256'
-  checksum64    = 'EC123F7A2BB7D0EC2B0F176A1BB7F855137BB4423CD78560E9C0C07C2EB5692C'
-  checksumType64= 'sha256'
 
   silentArgs   = '/SP- /VERYSILENT /NORESTART'
   validExitCodes= @(0)
